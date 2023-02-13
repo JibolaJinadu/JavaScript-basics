0c0502d69bd64bd2466196471d0895932ec1e73e
Jibola Jinadu <Jibolajinadus8@gmail.com>
2023-02-13 19:14:26 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//Multiplication function
+let a = 11;
+let b = 8;
+function multiply (){console.log(a*b)}
+multiply()
+
+
+//Creating variables
+let firstName = 'Jibola';
+let lastName = 'Jinadu';
+let stutern_Track = 'Front-end Engineer';
+let skills = 'Problem-solving, Leadership, Microsoft-office tools, Programming';
+let favouriteColor = 'Brown';
+let yearOfIndependence = 1960;
+
+console.log(firstName);
+console.log(lastName);
+console.log(stutern_Track);
+console.log(skills);
+console.log(favouriteColor);
+console.log(yearOfIndependence);
+
+//Construct sentence
+console.log("My name is  "+ firstName +" "+ lastName +", I am part of Stutern cohort 1.4, "+ stutern_Track +", some of my expertise include "+ skills +". Meanwhile, my favourite color is "+ favouriteColor +", and my dear country got her Independence in the year "+ yearOfIndependence +".")
+            
+            
+             
+            
+
